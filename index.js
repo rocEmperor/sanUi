2bc16414bf018799d3266f83d957394bb51ae051
lupp <m17710499106_1@163.com>
2020-10-26 14:43:30 +0800

    favicon.ico

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = new express();
 const port = 9986;
 // 静态文件
 app.use('/sanUi', express.static('public'));
+app.use(express.static('public'));
 
 app.listen(port, () => {
     console.log(`server start on 127.0.0.1:${port}`);
